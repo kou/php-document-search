765d9980def91652862fb630f402084f563534a0
Kouhei Sutou <kou@clear-code.com>
2017-06-26 11:36:38 +0900

    Add Entry
        % php artisan make:model --migration --controller --resource Entry

---
+++ database/migrations/2017_06_26_022843_create_entries_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEntriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entries', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('url');
+            $table->text('title');
+            $table->text('content');
+            $table->timestamps();
+            $table->index(['title', 'content'],
+                          null,
+                          'pgroonga');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entries');
+    }
+}
