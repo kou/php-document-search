14794a53cb1ad8d83244aa6e0282d7bfacbc22a4
Kouhei Sutou <kou@clear-code.com>
2017-06-26 11:06:57 +0900

    Add migration to enable PGroonga
        % php artisan make:migration enable_pgroonga

---
+++ database/migrations/2017_06_26_015917_enable_pgroonga.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class EnablePgroonga extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('CREATE EXTENSION pgroonga;');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('DROP EXTENSION pgroonga;');
+    }
+}
